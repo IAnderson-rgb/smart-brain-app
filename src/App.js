6caa9f247f2bdd8d66ee5c3aaab9fc3e05dc7e9d
Ianderson-rgb <afamilyenterprises@gmail.com>
2023-01-20 14:37:05 -0500

    added sign out nav and logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component} from 'react'
-import Navigation from './Components/Navigation/Navigation';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css'; 
 
-class App extends Component() {
+class App extends Component {
+
   render() {
   return (
-    <div className="App">
-     <Navigation />
-     {/* <Logo />
-     <ImageLinkForm />
+		<div className='App'>
+			<Navigation />
+			<Logo />
+			{/*  <ImageLinkForm />
      <FaceRecognition /> */}
-    </div>
-  );
+		</div>
+	);
 }
 }
 
 export default App;
+
