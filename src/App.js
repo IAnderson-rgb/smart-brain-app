1838a31ab4443fdd7bdd471f8ba9124ca3becc2c
Ianderson-rgb <afamilyenterprises@gmail.com>
2023-01-21 00:23:42 -0500

    added Rank component and additional styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 import './App.css'; 
 
 class App extends Component {
@@ -9,8 +11,11 @@ class App extends Component {
   render() {
   return (
 		<div className='App'>
+			{/* <ParticlesBg color="#E4EAEF" num={300} type="cobweb" bg={true} /> */}
+			<ParticlesBg type='circle' bg={true} />
 			<Navigation />
 			<Logo />
+			<Rank />
 			<ImageLinkForm />
 			{/*<FaceRecognition /> */}
 		</div>
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './ImageLinkForm.css'
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
 
@@ -9,9 +9,9 @@ const ImageLinkForm = () => {
 				{'This Magic Brain will detect faces in your pictures. Give it a try!'}
 			</p>
 			<div className='center'>
-				<div className='center form pa3 br3 shadow-5'>
+				<div className='center form pa3 br3 shadow-6'>
 					<input className='f4 pa2 w-70 center' type='text' />
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-black-40'>
+					<button className='w-30 grow f4 link ml2 ph3 pv2 dib white bg-black-40'>
 						Detect
 					</button>
 				</div>
